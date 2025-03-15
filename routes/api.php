b57f7c70cee1cbc868874ee5e9b5baab508afc3f
Lekito <alexdossantosdias1212@gmail.com>
2025-03-14 21:36:15 -0300

    protecting api routes

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/status', function(){
     return ApiResponse::success('API is running');
-});
+})->middleware('auth:sanctum');
 
-Route::apiResource('clients', ClientController::class);
+Route::apiResource('clients', ClientController::class)->middleware('auth:sanctum');
 
 //auth routes
 Route::post('/login', [AuthController::class, 'login']);
